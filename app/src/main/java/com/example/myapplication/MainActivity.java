392875bb6c06d81b34f7b26cb866977072635262
maoulito <mael.lebras29200@gmail.com>
2020-05-19 09:40:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+
+
+public class MainActivity extends Activity {
+
+    WebView web1;
+    EditText ed1;
+    Button bt1;
+    String Address;
+    ProgressBar pbar;
+    Button bt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main); web1 =(WebView)findViewById(R.id.webView1);
+        ed1 = (EditText)findViewById(R.id.editText1);
+        bt1 = (Button)findViewById(R.id.button1);
+        bt2 = (Button)findViewById(R.id.screen);
+        pbar = (ProgressBar)findViewById(R.id.progressBar1);
+        pbar.setVisibility(View.GONE);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Address = "http://" + ed1.getText().toString();
+                WebSettings webSetting = web1.getSettings();
+                webSetting.setBuiltInZoomControls(true);
+                webSetting.setJavaScriptEnabled(true);
+                webSetting.setLoadsImagesAutomatically(true);
+
+
+                web1.setWebViewClient(new WebViewClient());
+
+                web1.loadUrl(Address);
+
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenshot();
+            }
+        });
+
+    }
+
+    public class WebViewClient extends android.webkit.WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            pbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            pbar.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && web1.canGoBack()) {
+            web1.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void screenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("hh:mm:ss_dd_MM_yyyy", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or DOM
+            e.printStackTrace();
+        }
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+}
